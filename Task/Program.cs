2188ad13c1e6f087c6bdf7ee0dd17db871c12f67
Timur <arbo1234@yandex.ru>
2022-08-02 21:28:47 +0300

    Добавьте файлы проекта.

---
+++ Task/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] mass = { "Hello",  "Привет",  "How are you",  "Hi", "Bye" };
+string[] GetNewMassIf (string[] mass)
+{
+    string [] newMass = new string[mass.Length];
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i].Length <= 3)
+        {
+            newMass[i] = mass[i];
+        }
+    }
+    return newMass;
+}
+void PrintArray(string[] mass)
+{
+    foreach (string i in mass)
+        Console.Write($"{i} ");
+}
+string [] newMass = GetNewMassIf(mass);
+PrintArray(newMass);
